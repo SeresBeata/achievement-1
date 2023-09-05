253a026c6c973f517764c2212faae095c54f5854
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-09-05 10:12:04 +0200

    Update real-time form validation
    Update real-time form validation with showErrorMessage() function in order to append or remove error message.

--- js/contact.js
+++ js/contact.js
@@ -6,4 +6,23 @@ let formValidation = (() => {
   let firstNameInput = document.querySelector("#firstName");
   let lastNameInput = document.querySelector("#lastName");
   let emailInput = document.querySelector("#emailAddress");
+
+  //Create function to append/remove error message
+  function showErrorMessage(input, message) {
+    let container = input.parentElement; // The parent <div> of inputs
+
+    // Check and Remove any existing errors
+    let error = container.querySelector(".error-message");
+    if (error) {
+      container.removeChild(error);
+    }
+
+    // Now add the error if the message isn’t empty
+    if (message) {
+      let error = document.createElement("div");
+      error.classList.add("error-message");
+      error.innerText = message;
+      container.appendChild(error);
+    }
+  }
 })();
