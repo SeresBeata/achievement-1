3a98829a3f979c91e097ddc25dbd5c645c417cde
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-09-05 10:20:07 +0200

    Create validateEmail() function
    Update IIFE with validateEmail() function in order to validate the value of the "Email address" input.

--- js/contact.js
+++ js/contact.js
@@ -51,4 +51,27 @@ let formValidation = (() => {
     showErrorMessage(lastNameInput, null);
     return true;
   }
+
+  //Create function for validation of the email address
+  function validateEmail() {
+    let value = emailInput.value;
+
+    if (!value) {
+      showErrorMessage(emailInput, "Email is a required field.");
+      return false;
+    }
+
+    if (value.indexOf("@") === -1) {
+      showErrorMessage(emailInput, "You must enter a valid email address.");
+      return false;
+    }
+
+    if (value.indexOf(".") === -1) {
+      showErrorMessage(emailInput, "You must enter a valid email address.");
+      return false;
+    }
+
+    showErrorMessage(emailInput, null);
+    return true;
+  }
 })();
