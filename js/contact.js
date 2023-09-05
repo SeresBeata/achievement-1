911af662edf2f385068873f8e9fe304010d76f1a
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-09-05 10:08:17 +0200

    Create real-time form validation
    Update contact.js file with IIFE, within create variables for the form and the inputs of the contact.html file.

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+//Create Real-Time Form Validation for contact.html
+
+let formValidation = (() => {
+  //Create variables for the form and inputs
+  let form = document.querySelector(".contact-main-section__form");
+  let firstNameInput = document.querySelector("#firstName");
+  let lastNameInput = document.querySelector("#lastName");
+  let emailInput = document.querySelector("#emailAddress");
+})();
