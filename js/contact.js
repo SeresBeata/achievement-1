ea98d4dc6c507c0cc2dcc6d059b90abe2ff2098d
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-08 13:57:44 +0100

    Comment-out code from contact.js
    Comment-out the code of  Real-Time Contact Form Validation at contact.js file.
    Comment-out <script> element for contact.js at contact.html file.

--- js/contact.js
+++ js/contact.js
@@ -1,99 +1,99 @@
-//Create Real-Time Form Validation for contact.html
+// //Create Real-Time Form Validation for contact.html
 
-let formValidation = (() => {
-  //Create variables for the form and inputs
-  let form = document.querySelector(".contact-main-section__form");
-  let firstNameInput = document.querySelector("#firstName");
-  let lastNameInput = document.querySelector("#lastName");
-  let emailInput = document.querySelector("#emailAddress");
+// let formValidation = (() => {
+//   //Create variables for the form and inputs
+//   let form = document.querySelector(".contact-main-section__form");
+//   let firstNameInput = document.querySelector("#firstName");
+//   let lastNameInput = document.querySelector("#lastName");
+//   let emailInput = document.querySelector("#emailAddress");
 
-  //Create function to append/remove error message
-  function showErrorMessage(input, message) {
-    let container = input.parentElement; // The parent <div> of inputs
+//   //Create function to append/remove error message
+//   function showErrorMessage(input, message) {
+//     let container = input.parentElement; // The parent <div> of inputs
 
-    // Check and Remove any existing errors
-    let error = container.querySelector(".error-message");
-    if (error) {
-      container.removeChild(error);
-    }
+//     // Check and Remove any existing errors
+//     let error = container.querySelector(".error-message");
+//     if (error) {
+//       container.removeChild(error);
+//     }
 
-    // Now add the error if the message isn’t empty
-    if (message) {
-      let error = document.createElement("div");
-      error.classList.add("error-message");
-      error.innerText = message;
-      container.appendChild(error);
-    }
-  }
+//     // Now add the error if the message isn’t empty
+//     if (message) {
+//       let error = document.createElement("div");
+//       error.classList.add("error-message");
+//       error.innerText = message;
+//       container.appendChild(error);
+//     }
+//   }
 
-  //Create function for validation of the first name
-  function validateFirstName() {
-    let value = firstNameInput.value;
+//   //Create function for validation of the first name
+//   function validateFirstName() {
+//     let value = firstNameInput.value;
 
-    if (!value) {
-      showErrorMessage(firstNameInput, "First name is a required field.");
-      return false;
-    }
+//     if (!value) {
+//       showErrorMessage(firstNameInput, "First name is a required field.");
+//       return false;
+//     }
 
-    showErrorMessage(firstNameInput, null);
-    return true;
-  }
+//     showErrorMessage(firstNameInput, null);
+//     return true;
+//   }
 
-  //Create function for validation of the last name
-  function validateLastName() {
-    let value = lastNameInput.value;
+//   //Create function for validation of the last name
+//   function validateLastName() {
+//     let value = lastNameInput.value;
 
-    if (!value) {
-      showErrorMessage(lastNameInput, "Last name is a required field.");
-      return false;
-    }
+//     if (!value) {
+//       showErrorMessage(lastNameInput, "Last name is a required field.");
+//       return false;
+//     }
 
-    showErrorMessage(lastNameInput, null);
-    return true;
-  }
+//     showErrorMessage(lastNameInput, null);
+//     return true;
+//   }
 
-  //Create function for validation of the email address
-  function validateEmail() {
-    let value = emailInput.value;
+//   //Create function for validation of the email address
+//   function validateEmail() {
+//     let value = emailInput.value;
 
-    if (!value) {
-      showErrorMessage(emailInput, "Email is a required field.");
-      return false;
-    }
+//     if (!value) {
+//       showErrorMessage(emailInput, "Email is a required field.");
+//       return false;
+//     }
 
-    if (value.indexOf("@") === -1) {
-      showErrorMessage(emailInput, "You must enter a valid email address.");
-      return false;
-    }
+//     if (value.indexOf("@") === -1) {
+//       showErrorMessage(emailInput, "You must enter a valid email address.");
+//       return false;
+//     }
 
-    if (value.indexOf(".") === -1) {
-      showErrorMessage(emailInput, "You must enter a valid email address.");
-      return false;
-    }
+//     if (value.indexOf(".") === -1) {
+//       showErrorMessage(emailInput, "You must enter a valid email address.");
+//       return false;
+//     }
 
-    showErrorMessage(emailInput, null);
-    return true;
-  }
+//     showErrorMessage(emailInput, null);
+//     return true;
+//   }
 
-  //Create function for validation of the form
-  function validateForm() {
-    let isValidFirstName = validateFirstName();
-    let isValidLastName = validateLastName();
-    let isValidEmail = validateEmail();
+//   //Create function for validation of the form
+//   function validateForm() {
+//     let isValidFirstName = validateFirstName();
+//     let isValidLastName = validateLastName();
+//     let isValidEmail = validateEmail();
 
-    return isValidFirstName && isValidLastName && isValidEmail;
-  }
+//     return isValidFirstName && isValidLastName && isValidEmail;
+//   }
 
-  //Add eventListener to the form
-  form.addEventListener("submit", (e) => {
-    e.preventDefault(); // Do not submit to the server
-    if (validateForm()) {
-      alert("Success!");
-    }
-  });
+//   //Add eventListener to the form
+//   form.addEventListener("submit", (e) => {
+//     e.preventDefault(); // Do not submit to the server
+//     if (validateForm()) {
+//       alert("Success!");
+//     }
+//   });
 
-  //to validate the fields once the user starts typing in them
-  firstNameInput.addEventListener("input", validateFirstName);
-  lastNameInput.addEventListener("input", validateLastName);
-  emailInput.addEventListener("input", validateEmail);
-})();
+//   //to validate the fields once the user starts typing in them
+//   firstNameInput.addEventListener("input", validateFirstName);
+//   lastNameInput.addEventListener("input", validateLastName);
+//   emailInput.addEventListener("input", validateEmail);
+// })();
